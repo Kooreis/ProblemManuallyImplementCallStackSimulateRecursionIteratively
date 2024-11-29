df1e87c750aef5746673e21abd6954bdbbbc6ffa
Kooreis <kooreis.official@gmail.com>
2024-11-29 16:23:18 -0600

    This update adds four methods to the Stack class: push, pop, peek, and isEmpty. The push method adds an element to the stack, the pop method removes and returns the top element of the stack, the peek method returns the top element without removing it, and the isEmpty method checks if the stack is empty.

--- solution.cs
+++ solution.cs
@@ -1,3 +1,18 @@
-function Stack() {
-    this.stack = [];
+Stack.prototype.push = function(value) {
+    this.stack.push(value);
+}
+
+Stack.prototype.pop = function() {
+    if (this.stack.length === 0) {
+        return 'Underflow';
+    }
+    return this.stack.pop();
+}
+
+Stack.prototype.peek = function() {
+    return this.stack[this.stack.length - 1];
+}
+
+Stack.prototype.isEmpty = function() {
+    return this.stack.length === 0;
 }
